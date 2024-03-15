0901ed599f9636f6d8c73bc0c90d7f9f77986e07
Saurav <sauravsingh2205@gmail.com>
2024-03-15 07:14:16 +0530

    if else

---
+++ ifelse.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+int main() {
+    int x = 45 ;
+    int y = 18 ; 
+    if(x > y ) {
+        cout << "x is greater than y" ;
+    }
+    else {
+        cout << "x is less than y" ;
+    }
+  
+  return 0;
+    
+}
