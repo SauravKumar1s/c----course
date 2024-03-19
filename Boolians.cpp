dfc5c184c50f6a0163257310eafe637980b954be
Saurav <sauravsingh2205@gmail.com>
2024-03-19 07:58:16 +0530

    about Boolian

---
+++ Boolians.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+  int x = 10 ;
+  int y = 5;
+
+  cout << ( x > y );
+//   cout << ( x < y );
+  return 0;
+}
+
+// here is that is true then will return 0 if not then will return 1 
