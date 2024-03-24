88a29a374e9899fca54818a70969e16fd4b5e81d
Saurav <sauravsingh2205@gmail.com>
2024-03-24 08:28:01 +0530

    while and do while

--- loops.cpp
+++ loops.cpp
@@ -18,24 +18,49 @@
 #include <iostream>
 using namespace std;
 
+// int main()
+// {
+//     int userInput;
+
+//     cout << "Enter a number to print its multiplication table (1-10): ";
+//     cin >> userInput;
+
+//     if (userInput >= 1 && userInput <= 10)
+//     {
+//         for (int i = 1; i < 11; i++)
+//         {
+//             cout << userInput << " x " << i << " = " << userInput * i << "\n";
+//         }
+//     }
+//     else
+//     {
+//         cout << "Invalid input. Please enter a number between 1 and 10.\n";
+//     }
+
+//     return 0;
+// }
+
+// while loop
+// int main(){
+//     int i = 0;
+//     while(i < 10) {
+//         cout << i << "\n";
+//         i++;
+
+//     }
+
+// }
+
+// do while loop
+
 int main()
 {
-    int userInput;
-
-    cout << "Enter a number to print its multiplication table (1-10): ";
-    cin >> userInput;
-
-    if (userInput >= 1 && userInput <= 10)
+   int i = 0;
+    do
     {
-        for (int i = 1; i < 11; i++)
-        {
-            cout << userInput << " x " << i << " = " << userInput * i << "\n";
-        }
-    }
-    else
-    {
-        cout << "Invalid input. Please enter a number between 1 and 10.\n";
-    }
 
-    return 0;
+        cout << i << "\n";
+        i++;
+    }
+    while (i < 15);
 }
