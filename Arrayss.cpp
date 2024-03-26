cfe76b052d60dd18ae666b032de9089ed1520210
Saurav <sauravsingh2205@gmail.com>
2024-03-26 07:36:44 +0530

    arrays day 1

---
+++ Arrayss.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string cars[3] = {"A", "B", "C"};
+    cars[0] = "Rohit";
+    // cout << cars[0]; 
+    cout << cars[2]; 
+    // return 0;
+
+}
