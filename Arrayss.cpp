3172090df92ce6ba14ed3551ff82abf8bcca9f18
Saurav <sauravsingh2205@gmail.com>
2024-03-30 07:41:54 +0530

    loop with arrays

--- Arrayss.cpp
+++ Arrayss.cpp
@@ -2,11 +2,23 @@
 #include <string>
 using namespace std;
 
-int main() {
-    string cars[3] = {"A", "B", "C"};
-    cars[0] = "Rohit";
-    // cout << cars[0]; 
-    cout << cars[2]; 
-    // return 0;
+// int main() {
+//     string cars[3] = {"A", "B", "C"};
+//     cars[0] = "Rohit";
+//     // cout << cars[0];
+//     cout << cars[2];
+//     // return 0;
 
+// }
+
+// loop with arrays
+
+int main()
+{
+    string cars[3] = {"A", "B", "C"};
+    for (int i = 0; i < 3; i++)
+    {
+        cout << cars[i] << " \n";
+    }
+    return 0;
 }
