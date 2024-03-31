b47fd99621d40676bffeb6170c0ccb990144c272
Saurav <sauravsingh2205@gmail.com>
2024-03-31 07:31:04 +0530

    2d arrays

--- Arrayss.cpp
+++ Arrayss.cpp
@@ -13,12 +13,55 @@ using namespace std;
 
 // loop with arrays
 
-int main()
-{
-    string cars[3] = {"A", "B", "C"};
-    for (int i = 0; i < 3; i++)
-    {
-        cout << cars[i] << " \n";
+// int main()
+// {
+//     string cars[3] = {"A", "B", "C"};
+//     for (int i = 0; i < 3; i++)
+//     {
+//         cout << cars[i] << " \n";
+//     }
+//     return 0;
+// }
+
+//  2d array
+
+// int main()
+// {
+//     int n, m;
+//     int arr[100][100];
+
+//     // Read the dimensions of the array
+//     cin >> n >> m;
+//     {
+//         for (int i = 0; i < n; i++)
+//         {
+//             for (int j = 0; j < m; i++)
+//             {
+//                 cin >> arr[i][j];
+//             }
+//         }
+//     }
+// }
+
+#include <iostream> // Necessary for input/output operations
+
+using namespace std;
+
+int main() {
+    int n, m;
+    int arr[100][100];
+
+    // Read the dimensions of the array
+    cin >> n >> m;
+
+    // Read the elements of the array
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) { // Correct the loop variable to j++
+            cin >> arr[i][j];
+        }
     }
-    return 0;
+
+    // Your code for processing the array goes here
+
+    return 0; // Indicate successful execution
 }
